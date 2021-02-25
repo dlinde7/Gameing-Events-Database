9c45a87f1b2e56b5e9670df672737470ef3bb0fc
Kyle Twomey <39479526+Kyle-2may@users.noreply.github.com>
2021-02-25 16:56:48 +0200

    Add files via upload

---
+++ DATA/AccountTypes.sql
@@ -0,0 +1,7 @@
+﻿INSERT INTO [dbo].[AccountTypes] (Name, Description, AddedBy, AddedByDateTime, LastUpdatedBy, LastUpdatedByDateTime)
+VALUES 
+((Admin,The account type specific to admin users,4,SYSDATE(),4,SYSDATE()),
+(Developer,The account type specific to developer users,4,SYSDATE(),4,SYSDATE()),
+(Free,The account type specific to free users,4,SYSDATE(),4,SYSDATE()),
+(Platinum,The account type specific to platinum users,4,SYSDATE(),4,SYSDATE()),
+(Gold,The account type specific to gold users,4,SYSDATE(),4,SYSDATE()))
